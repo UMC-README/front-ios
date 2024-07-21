59be0f1d207e7f36a3c95a4ca793dea132aa7c88
realhsb <realhsb@gmail.com>
2024-07-21 19:30:00 +0900

    chore: 프로젝트 파일 생성

---
+++ Readme-ios/Readme/Readme/ReadmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadmeApp.swift
+//  Readme
+//
+//  Created by Subeen on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ReadmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
