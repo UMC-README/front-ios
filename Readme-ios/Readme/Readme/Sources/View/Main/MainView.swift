1dfb9ab05720fc2b1a8735d4fbeadd28fd4d3bb7
realhsb <realhsb@gmail.com>
2024-07-22 15:05:06 +0900

    feat: 로그인 여부에 따른 화면 분기 처리

---
+++ Readme-ios/Readme/Readme/Sources/View/Main/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Readme
+//
+//  Created by Subeen on 7/22/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("MainView")
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ Readme-ios/Readme/Readme/Sources/View/SignIn/SigninIntroView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInIntroView.swift
+//  Readme
+//
+//  Created by Subeen on 7/22/24.
+//
+
+import SwiftUI
+
+struct SigninIntroView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    SigninIntroView()
+}
