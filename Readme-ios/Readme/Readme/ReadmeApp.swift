871f13ce0670cb25ea4ea32e693b5ed87666088d
realhsb <realhsb@gmail.com>
2024-07-22 15:01:43 +0900

    feat: DIContainer 및 Services 연결

--- Readme-ios/Readme/Readme/ReadmeApp.swift
+++ Readme-ios/Readme/Readme/ReadmeApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct ReadmeApp: App {
+    
+    @StateObject var container: DIContainer = .init(services: Services())
+    
     var body: some Scene {
         WindowGroup {
-//            ContentView()
+            AuthenticatedView(authViewModel: AuthentificationViewModel(container: container))
         }
     }
 }
