44b256a436266a678114cfdd1c8e8d720b60fbcb
realhsb <realhsb@gmail.com>
2024-07-27 04:40:07 +0900

    feat: 공지글 자세히보기 view

---
+++ Readme-ios/Readme/Readme/Sources/View/Notice/NoticeDetailView.swift
@@ -0,0 +1,82 @@
+//
+//  NoticeDetailView.swift
+//  Readme
+//
+//  Created by Subeen on 7/26/24.
+//
+
+import SwiftUI
+
+struct NoticeDetailView: View {
+    
+    var notice: Notice = Notice(title: "공지방 예시", content: "공지글 내용 예시입니다. 공지글 내용 예시입니다. 공지글 내용 예시입니다. 공지글 내용 예시입니다.", type: .mission)
+    
+    // TODO: Comment
+    
+    var body: some View {
+        ZStack {
+            Color.backgroundLight
+            
+            ScrollView {
+                contentView
+            }
+            
+        }
+    }
+    
+    
+    /// 공지글 본문 뷰
+    @ViewBuilder
+    var contentView: some View {
+        VStack(spacing: 8) {
+            HStack {
+                
+                NoticeTypeIconView(type: notice.type ?? .mission)
+                Spacer()
+                Text("댓글 버튼 개수")
+                Text("버튼")
+            }
+            
+            Text(notice.title ?? "title error")
+                .font(.pretendardBold18)
+                .foregroundStyle(Color.txtDefault)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            HStack {
+                Text("yy.MM.dd HH:MM")
+                Text("-")
+                Text("yy.MM.dd HH:MM")
+            }
+            .foregroundStyle(Color.primaryNormal)
+            .font(.pretendardRegular12)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.bottom, 8)
+            .border(width: 0.33, edges: [.bottom], color: .primaryNormal)
+            
+            HStack {
+                Text(notice.content ?? "content error")
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
+                    .multilineTextAlignment(.leading)
+                    .font(.pretendardMedium16)
+                    .foregroundStyle(Color.txtCaption)
+            }
+        }
+        .padding(10)
+        .background(Color.primaryLight)
+        .clipShape(RoundedRectangle(cornerRadius: 8))
+    }
+    
+    /// 댓글창 뷰
+    @ViewBuilder
+    var commentView: some View {
+        VStack {
+            Text("댓글")
+        }
+    }
+}
+
+#Preview {
+    NoticeDetailView()
+}
+
+
