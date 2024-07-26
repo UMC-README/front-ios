fca4e0d812a8187cb0ebe4cca9e8302b5450e835
realhsb <realhsb@gmail.com>
2024-07-27 04:36:01 +0900

    feat: DIContainer - ServiceType, NavigationRouter 주입

--- Readme-ios/Readme/Readme/Sources/General/DIContainer.swift
+++ Readme-ios/Readme/Readme/Sources/General/DIContainer.swift
@@ -9,8 +9,19 @@ import Foundation
 
 class DIContainer: ObservableObject {
     var services: ServiceType
+    var navigationRouter: NavigationRoutable
     
-    init(services: ServiceType) {
+    init(
+        services: ServiceType,
+        navigationRouter: NavigationRoutable = NavigationRouter()
+    ) {
         self.services = services
+        self.navigationRouter = navigationRouter
+    }
+}
+
+extension DIContainer {
+    static var stub: DIContainer {
+        .init(services: StubServices())
     }
 }
