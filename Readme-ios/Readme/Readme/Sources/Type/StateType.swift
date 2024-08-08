d8a1d31a73313009a3c6ecacd5a16b7024f66922
realhsb <realhsb@gmail.com>
2024-08-08 15:54:46 +0900

    chore: 공지방 상태값 enum 및 상태에 따른 배경색 설정

--- Readme-ios/Readme/Readme/Sources/Type/StateType.swift
+++ Readme-ios/Readme/Readme/Sources/Type/StateType.swift
@@ -5,10 +5,31 @@
 //  Created by Subeen on 7/23/24.
 //
 
-import Foundation
+import SwiftUI
 
 /// 존재 여부 State
-enum StateType: Codable {
-    case exist
-    case deleted
+enum StateType: String, Codable {
+    case exist = "EXIST"
+    case deleted = "DELETED"
+    
+//    var description: String {
+//        switch self {
+//        case .deleted:
+//            "삭제된 공지방"
+//        case .exist:
+//            ""
+//        }
+//    }
+    
+    /// 아이콘 배경 색상
+    var backColor: SwiftUI.Color {
+        switch self {
+        case .exist:
+            Color.primaryNormal
+        case .deleted:
+            Color.danger
+        }
+    }
 }
+
+
