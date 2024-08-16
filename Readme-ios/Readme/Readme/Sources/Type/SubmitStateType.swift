1ab8cdd02aaa5739659f23e1978466a021e91ff0
realhsb <realhsb@gmail.com>
2024-08-17 03:46:24 +0900

    feat: 공지 정답 제출 여부 상태에 따른 글자색 및 배경색 지정

--- Readme-ios/Readme/Readme/Sources/Type/SubmitStateType.swift
+++ Readme-ios/Readme/Readme/Sources/Type/SubmitStateType.swift
@@ -5,7 +5,7 @@
 //  Created by Subeen on 7/23/24.
 //
 
-import Foundation
+import SwiftUI
 
 /// 제출 상태 State
 enum SubmitStateType: String, Codable {
@@ -24,6 +24,34 @@ enum SubmitStateType: String, Codable {
             "거절"
         case .notComplete:
             "미제출"
+        }
+    }
+    
+    /// 아이콘 폰트 색상
+    var foreColor: SwiftUI.Color {
+        switch self {
+        case .complete:
+            Color.success
+        case .pending:
+            Color.txtDefault
+        case .reject:
+            Color.danger
+        case .notComplete:
+            Color.txtCaption
+        }
+    }
+    
+    /// 아이콘 배경 색상
+    var backColor: SwiftUI.Color {
+        switch self {
+        case .complete:
+            Color.successLight
+        case .pending:
+            Color.gray2
+        case .reject:
+            Color.dangerLight
+        case .notComplete:
+            Color.gray2
         }
     }
 }
